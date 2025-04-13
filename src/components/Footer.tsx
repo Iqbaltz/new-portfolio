a4af08fa6829950aefd7e3615ffe6528eccc0012
Iqbal <mhdiqbaltan@gmail.com>
2025-04-13 16:57:39 +0700

    minor changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,11 +20,11 @@ const Footer = () => {
           Ready to take <span className="text-purple">your</span> digital
           presence to the next level?
         </h1>
-        <p className="my-5 md:mt-10 text-center text-white-200">
+        <p className="my-5 md:mt-10 text-white-200 text-center">
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="mailto:contact@jsmastery.pro">
+        <a href="mailto:mhdiqbaltan@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
@@ -41,7 +41,7 @@ const Footer = () => {
           {socialMedia.map((info) => (
             <div
               key={info.id}
-              className="flex justify-center items-center bg-black-200 bg-opacity-75 backdrop-blur-lg backdrop-filter border border-black-300 rounded-lg w-10 h-10 cursor-pointer saturate-180"
+              className="flex justify-center items-center bg-black-200 bg-opacity-75 backdrop-filter backdrop-blur-lg saturate-180 border border-black-300 rounded-lg w-10 h-10 cursor-pointer"
             >
               <img src={info.img} alt="icons" width={20} height={20} />
             </div>
--- src/data/index.ts
+++ src/data/index.ts
@@ -196,29 +196,29 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
+    title: "Fullstack Developer - PT Lion Superindo",
     desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
     className: "md:col-span-2",
     thumbnail: "/assets/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Front End Developer - STRT Button",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/assets/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Front End Developer - Mejakreatif",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/assets/exp3.svg",
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Freelance Web Dev Project",
+    desc: "Led the dev of a web app for a client, from initial concept to deployment.",
     className: "md:col-span-2",
     thumbnail: "/assets/exp4.svg",
   },
